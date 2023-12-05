ecfab4dcd6e296da013dce83ef0cd4f7d3b85e66
john <jarrington08@gmail.com>
2023-12-04 23:19:56 -0600

    Initialized react and spring apps

---
+++ bookshelf-corner-spring/src/main/java/org/launchcode/bookshelfcorner/BookshelfCornerApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.bookshelfcorner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshelfCornerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookshelfCornerApplication.class, args);
+	}
+
+}
---
+++ bookshelf-corner-spring/src/test/java/org/launchcode/bookshelfcorner/BookshelfCornerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.bookshelfcorner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshelfCornerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
