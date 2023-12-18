6020a63033b6dfe80e92816e83c9a76677bda9a3
john <jarrington08@gmail.com>
2023-12-18 16:31:17 -0600

    Successfully linked up React and Spring. Tested with POST request to add data to MySQL database

--- bookshelf-corner-react/src/App.js
+++ bookshelf-corner-react/src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header';
+import AddUser from './users/AddUser';
 import './App.css';
 
 function App() {
   return (
     <div>
       <Header />
+      <AddUser />
     </div>
   );
 }
