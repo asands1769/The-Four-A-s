e00a47a3551a1c99f8163c3db9bd66ab781f6ab3
Tarekegne <tarinium@yahoo.com>
2023-12-18 18:16:29 -0500

    add annotation in the controller class to connect with the front end

--- bookshelf-corner-spring/src/main/java/org/launchcode/bookshelfcorner/controller/BookListController.java
+++ bookshelf-corner-spring/src/main/java/org/launchcode/bookshelfcorner/controller/BookListController.java
@@ -3,13 +3,15 @@ package org.launchcode.bookshelfcorner.controller;
 import org.launchcode.bookshelfcorner.model.BookList;
 import org.launchcode.bookshelfcorner.repository.BookListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-//@RestController
+@CrossOrigin(origins = "http://localhost:3000/")
+@RestController
 @RequestMapping("/api")
 public class BookListController {
     @Autowired
