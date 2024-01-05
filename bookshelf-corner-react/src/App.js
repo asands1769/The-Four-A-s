ed33d9b9b841b35c4a1fa730abe577d3ffe6e364
john <jarrington08@gmail.com>
2024-01-04 19:48:56 -0600

    added router to app.js, made login.js and register.js

--- bookshelf-corner-react/src/App.js
+++ bookshelf-corner-react/src/App.js
@@ -1,12 +1,20 @@
 import Header from './components/Header';
-import AddUser from './users/AddUser';
+import AddUser from './users/Register';
+import Login from './users/Login';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Router>
+        <Routes>
+          <Route exact path="/login" element={<BookListComponent/>}></Route>
+        </Routes>
+      </Router>
       <Header />
-      <AddUser />
+      <Login />
     </div>
   );
 }
---
+++ bookshelf-corner-spring/src/main/java/org/launchcode/bookshelfcorner/controllers/LoginController.java
@@ -0,0 +1,2 @@
+package org.launchcode.bookshelfcorner.controllers;public class LoginController {
+}
