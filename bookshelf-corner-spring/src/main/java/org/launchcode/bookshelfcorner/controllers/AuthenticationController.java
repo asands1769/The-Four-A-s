f3786092b30c5a388f53ec832ab3d8a0a6bb1988
john <jarrington08@gmail.com>
2024-01-06 15:07:34 -0600

    Registration input validation is working. Valid users are successfully added to database. Need to work on correct ID gen, verify username and email available. Also, front-end validation for verifyPassword

--- bookshelf-corner-spring/src/main/java/org/launchcode/bookshelfcorner/controllers/AuthenticationController.java
+++ bookshelf-corner-spring/src/main/java/org/launchcode/bookshelfcorner/controllers/AuthenticationController.java
@@ -1,130 +1,43 @@
-//package org.launchcode.bookshelfcorner.controllers;
-//
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpSession;
-//import jakarta.validation.Valid;
-//import org.launchcode.bookshelfcorner.models.User;
-//import org.launchcode.bookshelfcorner.repository.UserRepository;
-//import org.launchcode.bookshelfcorner.models.dto.LoginFormDTO;
-//import org.launchcode.bookshelfcorner.models.dto.RegisterFormDTO;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.validation.Errors;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.ModelAttribute;
-//import org.springframework.web.bind.annotation.PostMapping;
-//
-//import java.util.Optional;
-//
-//@Controller
-//public class AuthenticationController {
-//
-//    @Autowired
-//    UserRepository userRepository;
-//
-//    private static final String userSessionKey = "user";
-//
-//    public User getUserFromSession(HttpSession session) {
-//        Integer userId = (Integer) session.getAttribute(userSessionKey);
-//        if (userId == null) {
-//            return null;
-//        }
-//
-//        Optional<User> user = userRepository.findById((userId));
-//
-//        if (user.isEmpty()) {
-//            return null;
-//        }
-//
-//        return user.get();
-//    }
-//
-//    private static void setUserInSession(HttpSession session, User user) {
-//        session.setAttribute(userSessionKey, user.getId());
-//    }
-//
-//    @GetMapping("/register")
-//    public String displayRegistrationForm(Model model) {
-//        model.addAttribute(new RegisterFormDTO());
-//        model.addAttribute("title", "Register");
-//        return "register";
-//    }
-//
-//    @PostMapping("/register")
-//    public String processRegistrationForm(@ModelAttribute @Valid RegisterFormDTO registerFormDTO,
-//                                          Errors errors, HttpServletRequest request,
-//                                          Model model) {
-//
-//        if (errors.hasErrors()) {
-//            model.addAttribute("title", "Register");
-//            return "register";
-//        }
-//
-//        User existingUser = userRepository.findByUsername(registerFormDTO.getUsername());
-//
-//        if (existingUser != null) {
-//            errors.rejectValue("username", "username.alreadyexists", "A user with that username already exists");
-//            model.addAttribute("title", "Register");
-//            return "register";
-//        }
-//
-//        String password = registerFormDTO.getPassword();
-//        String verifyPassword = registerFormDTO.getVerifyPassword();
-//        if (!password.equals(verifyPassword)) {
-//            errors.rejectValue("password", "passwords.mismatch", "Passwords do not match");
-//            model.addAttribute("title", "Register");
-//            return "register";
-//        }
-//
-//        User newUser = new User(registerFormDTO.getUsername(), registerFormDTO.getEmail(), registerFormDTO.getPassword());
-//        userRepository.save(newUser);
-//        setUserInSession(request.getSession(), newUser);
-//
-//        return "redirect:";
-//    }
-//
-//    @GetMapping("/login")
-//    public String displayLoginForm(Model model) {
-//        model.addAttribute(new LoginFormDTO());
-//        model.addAttribute("title", "Log In");
-//        return "login";
-//    }
-//
-//    @PostMapping("/login")
-//    public String processLoginForm(@ModelAttribute @Valid LoginFormDTO loginFormDTO,
-//                                   Errors errors, HttpServletRequest request,
-//                                   Model model) {
-//
-//        if (errors.hasErrors()) {
-//            model.addAttribute("title", "Log In");
-//            return "login";
-//        }
-//
-//        User theUser = userRepository.findByUsername(loginFormDTO.getUsername());
-//
-//        if (theUser == null) {
-//            errors.rejectValue("username", "user.invalid", "The given username does not exist");
-//            model.addAttribute("title", "Log In");
-//            return "login";
-//        }
-//
-//        String password = loginFormDTO.getPassword();
-//
-//        if (!theUser.isMatchingPassword(password)) {
-//            errors.rejectValue("password", "password.invalid", "Invalid password");
-//            model.addAttribute("title", "Log In");
-//            return "login";
-//        }
-//
-//        setUserInSession(request.getSession(), theUser);
-//
-//        return "redirect:";
-//    }
-//
-//    @GetMapping("/logout")
-//    public String logout(HttpServletRequest request){
-//        request.getSession().invalidate();
-//        return "redirect:/login";
-//    }
-//}
+package org.launchcode.bookshelfcorner.controllers;
+
+import jakarta.validation.Valid;
+
+import org.launchcode.bookshelfcorner.models.User;
+import org.launchcode.bookshelfcorner.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.launchcode.bookshelfcorner.models.dto.LoginRequestDTO;
+import org.launchcode.bookshelfcorner.models.dto.LoginResponseDTO;
+import org.launchcode.bookshelfcorner.models.dto.RegisterRequestDTO;
+
+@RestController
+//@RequestMapping("/login")
+@CrossOrigin(origins = "http://localhost:3000")
+public class AuthenticationController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/register")
+//    User newUser(@RequestBody User newUser) {
+//        return userRepository.save(newUser);
+    public ResponseEntity<LoginResponseDTO> performLogin(@Valid @RequestBody RegisterRequestDTO registerRequestDTO){
+        //How do we handle passwords not matching? Answer: client side. Add error message or alert and prevent from clicking on submit button? New span that shows error message or shared span with other error?
+        //How do we save to database?
+        User newUser = new User(registerRequestDTO.getUsername(), registerRequestDTO.getEmail(), registerRequestDTO.getPassword());
+        userRepository.save(newUser);
+        return ResponseEntity.ok(new LoginResponseDTO("Success !"));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> performLogin(@Valid @RequestBody LoginRequestDTO loginRequestDTO){
+        return ResponseEntity.ok(new LoginResponseDTO("Success !"));
+    }
+}
--- bookshelf-corner-spring/src/main/java/org/launchcode/bookshelfcorner/models/dto/RegisterRequestDTO.java
+++ bookshelf-corner-spring/src/main/java/org/launchcode/bookshelfcorner/models/dto/RegisterRequestDTO.java
@@ -5,22 +5,22 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 
-public class RegisterFormDTO extends LoginRequestDTO {
+public class RegisterRequestDTO extends LoginRequestDTO {
 
     @NotNull(message = "Username is required.")
     @NotBlank(message = "Username is required.")
     @Size(min = 3, message = "Invalid username. Must be between 3 and 20 characters.")
     private String username;
 
-    private String verifyPassword;
-
-    public String getVerifyPassword() {
-        return verifyPassword;
-    }
-
-    public void setVerifyPassword(String verifyPassword) {
-        this.verifyPassword = verifyPassword;
-    }
+//    private String verifyPassword;
+//
+//    public String getVerifyPassword() {
+//        return verifyPassword;
+//    }
+//
+//    public void setVerifyPassword(String verifyPassword) {
+//        this.verifyPassword = verifyPassword;
+//    }
 
     public String getUsername() { return username; }
 
