c5827410256681e9d53814c1e6542167799259ae
ats <atsumitha@gmailcom>
2024-01-11 18:44:07 -0600

    update commit

--- bookshelf-corner-spring/src/main/java/org/launchcode/bookshelfcorner/repository/TagRepository.java
+++ bookshelf-corner-spring/src/main/java/org/launchcode/bookshelfcorner/repository/TagRepository.java
@@ -1,10 +1,10 @@
-package org.launchcode.bookshelfcorner.repository;
-
-import org.launchcode.bookshelfcorner.models.Tag;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TagRepository extends CrudRepository<Tag, Integer> {
-
-}
+//package org.launchcode.bookshelfcorner.repository;
+//
+//import org.launchcode.bookshelfcorner.models.Tag;
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface TagRepository extends CrudRepository<Tag, Integer> {
+//
+//}
